2588c6f02908981df9b667cae698cbc9dc5b06cf
0$4M4 <oussama.louhaidia@gmail.com>
2024-09-04 21:18:19 +0700

    added search and filters

--- main.py
+++ main.py
@@ -4,81 +4,106 @@ import logging
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from collections import defaultdict
 from lib import *
 
 logging.basicConfig(level=logging.INFO)
 st.set_page_config(layout="wide")
 
+
 def fetch_market_sentiment(url):
     response = requests.get(url)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        selector = 'div > div > div:nth-of-type(2) > div:nth-of-type(1) > p:nth-of-type(2)'
+        soup = BeautifulSoup(response.content, "html.parser")
+        selector = (
+            "div > div > div:nth-of-type(2) > div:nth-of-type(1) > p:nth-of-type(2)"
+        )
         extracted_text = soup.select_one(selector).text
-        match = re.search(r'\d+%', extracted_text)
+        match = re.search(r"\d+%", extracted_text)
         if match:
-            percentage_value = float(match.group().strip('%'))
+            percentage_value = float(match.group().strip("%"))
 
             if percentage_value >= 75:
-                sentiment = "Extreme Greed" 
-                color_code = 'red'
+                sentiment = "Extreme Greed"
+                color_code = "red"
             elif 60 <= percentage_value < 75:
-                sentiment = "Greed 😨" 
-                color_code = 'orange'
+                sentiment = "Greed 😨"
+                color_code = "orange"
             elif 40 <= percentage_value < 60:
-                sentiment = "Neutral 😐" 
-                color_code = 'white'
+                sentiment = "Neutral 😐"
+                color_code = "white"
             elif 25 <= percentage_value < 40:
-                sentiment = "Fear 😏" 
-                color_code = 'yellow'
+                sentiment = "Fear 😏"
+                color_code = "yellow"
             else:
-                sentiment = "Extreme Fear" 
-                color_code = 'green'
+                sentiment = "Extreme Fear"
+                color_code = "green"
 
             return match.group(), sentiment, color_code
         else:
             logging.info("Failed to find the percentage in the extractor text.")
             return None, None, None
     else:
-        logging.error("Failed to retrieve the webpage - Status code: %s", response.status_code)
+        logging.error(
+            "Failed to retrieve the webpage - Status code: %s", response.status_code
+        )
         return None, None, None
 
+
 with st.sidebar:
-    url = 'https://pyinvesting.com/fear-and-greed/'
+    url = "https://pyinvesting.com/fear-and-greed/"
     percentage, sentiment, color_code = fetch_market_sentiment(url)
 
     if percentage and sentiment and color_code:
-        info_text = "% Stocks in the market that are in an uptrend trading above their 6 month exponential moving average (EMA)."
+        info_text = "Percentage of stocks in the market that are in an uptrend trading above their 6-month exponential moving average (EMA)."
         col1, col2 = st.columns(2)
 
         with col1:
             st.metric(label="Sentiment:", value=percentage, help=info_text)
 
         with col2:
-            st.markdown(f"<h1 style='color: {color_code};'>{sentiment}</h1>", unsafe_allow_html=True)
+            st.markdown(
+                f"<h1 style='color: {color_code};'>{sentiment}</h1>",
+                unsafe_allow_html=True,
+            )
 
-    days_history = st.number_input(
-        "Days History", min_value=365, max_value=36500, value=1825, step=365
+    # Converting days input to a dropdown for years
+    years_options = [1, 2, 3, 4, 5, 10]  # You can adjust the range as needed
+    years_selected = st.selectbox(
+        "Years History", years_options, index=4
+    )  # Default index can be adjusted
+    days_history = years_selected * 365
+    st.write(f"Days History: {days_history} days")
+
+    eps_threshold = st.number_input("EPS Threshold", value=2.0)
+    gross_margin_threshold = st.number_input("Gross Margin Threshold", value=0.7)
+    peg_threshold_low = st.number_input("PEG Lower Threshold", value=-0.1)
+    peg_threshold_high = st.number_input("PEG Upper Threshold", value=1.1)
+
+    st.subheader("Price Type Selection")
+    st.write(
+        "Select the type of price you want to analyze. Hover over each option for more details to help you decide."
     )
-    eps_threshold = st.number_input("EPS Threshold", value=5.0)
-    gross_margin_threshold = st.number_input("Gross Margin Threshold", value=0.8)
-    peg_threshold_low = st.number_input("PEG Lower Threshold", value=0.0)
-    peg_threshold_high = st.number_input("PEG Upper Threshold", value=1.0)
 
-with st.sidebar:
-    st.sidebar.subheader("Price Type Selection")
-    st.sidebar.write("Select the type of price you want to analyze. Hover over each option for more details to help you decide.")
-
+    options = [
+        ("poc_price", "Current Price below POC"),
+        ("disabled", "Disable Price Area Filter"),
+        ("va_high", "Current Price inside VA"),
+    ]
     option = st.radio(
         "Select the price threshold:",
-        options=[
-            ('va_high', 'Current Price inside VA'),
-            ('poc_price', 'Current Price below POC'), 
-            ('disabled', "Disable Price Area Filter")
-        ],
-        format_func=lambda x: x[1],  
-        help="Value Area High (va_high) refers to the highest price level within the Value Area where the majority of trading activity took place. \n\nPoint of Control Price (poc_price) is the price level for the time period with the highest traded volume."
+        options=options,
+        format_func=lambda x: x[1],
+        help="Value Area High (va_high) refers to the highest price level within the Value Area where the majority of trading activity took place. \n\nPoint of Control Price (poc_price) is the price level for the time period with the highest traded volume.",
     )
+
+    # Add a ticker search input field in the sidebar
+    search_ticker_1 = st.text_input(
+        "Search for a Ticker (use ';' to separate multiple tickers):",
+        key="search_ticker_1",
+    )
+    search_button_1 = st.button("Search", key="search_button_1")
+
 
 def init_session_state():
     if "companies" not in st.session_state:
@@ -89,6 +114,7 @@ def init_session_state():
         st.session_state.metrics = {}
     if "combined_metrics" not in st.session_state:
         st.session_state.combined_metrics = {}
+
 
 def display_metrics(metrics_dict):
     if not metrics_dict:
@@ -104,22 +130,222 @@ def display_metrics(metrics_dict):
         else:
             st.write(f"Value: {value}")
 
+
 def replace_with_zero(lst):
     if lst is None:
         return [0, 0, 0, 0]
 
-    return [0.0 if (pd.isna(x) or str(x).lower() == 'nan') else x for x in lst]
+    return [0.0 if (pd.isna(x) or str(x).lower() == "nan") else x for x in lst]
+
+
+def search_ticker_and_display(ticker, start_date_str, end_date_str, option):
+    try:
+        # Get the ticker object from Yahoo Finance
+        ticker_object = get_ticker_object(ticker)
+
+        # Fetch historical data for the given ticker
+        historical_data = fetch_historical_data(ticker, start_date_str, end_date_str)
+
+        if historical_data.empty:
+            st.error(f"No historical data found for ticker '{ticker}'.")
+            return
+
+        # Calculate market profile
+        va_high, va_low, poc_price, _ = calculate_market_profile(historical_data)
+
+        # Decision based on the selected price threshold option
+        current_price = ticker_object.info.get("currentPrice", None)
+        if current_price is None:
+            st.error(f"Current price data missing for ticker '{ticker}'.")
+            return
+
+        if option[0] == "va_high" and current_price > va_high:
+            logging.info(
+                f"{ticker} - Current price is above value area: {current_price} > {va_high}"
+            )
+            st.warning(
+                f"{ticker} - Current price is above the value area high ({va_high})."
+            )
+            return
+        elif option[0] == "poc_price" and current_price > poc_price:
+            logging.info(
+                f"{ticker} - Current price is above price of control: {current_price} > {poc_price}"
+            )
+            st.warning(
+                f"{ticker} - Current price is above the point of control ({poc_price})."
+            )
+            return
+
+        # Displaying the ticker details
+        website = ticker_object.info.get("website", "#")
+        short_name = ticker_object.info.get("shortName", ticker)
+        header_with_link = f"[🔗]({website}){short_name} - {ticker}"
+        st.markdown(f"### {header_with_link}", unsafe_allow_html=True)
+
+        # Display ticker financial metrics
+        display_ticker_metrics(ticker, ticker_object.info)
+
+        # Display news and sentiment
+        news_data, total_polarity = get_news_data(ticker)
+        col1_weight, col2_weight, col3_weight = 1, 2, 1
+        col1, col2, col3 = st.columns([col1_weight, col2_weight, col3_weight])
+
+        with col1:
+            display_sentiment_gauge(news_data, total_polarity)
+
+        with col2:
+            display_news_articles(news_data)
+
+        with col3:
+            plot_candle_chart_with_volume_profile(
+                historical_data, va_high, va_low, poc_price
+            )
+
+    except Exception as e:
+        st.error(
+            f"An error occurred while fetching and displaying ticker data for '{ticker}': {e}"
+        )
+        logging.error(f"Error in search_ticker_and_display: {e}")
+
+
+def display_ticker_metrics(ticker, info):
+    try:
+        metrics_labels = {
+            "pegRatio": "PEG Ratio",
+            "trailingEps": "EPS",
+            "trailingPE": "P/E Ratio",
+            "priceToSalesTrailing12Months": "P/S Ratio",
+            "priceToBook": "P/B Ratio",
+            "grossMargins": "Gross Margin (%)",
+            "marketCap": "Market Cap",
+        }
+
+        cols = st.columns(len(metrics_labels))
+
+        for col, (key, label) in zip(cols, metrics_labels.items()):
+            value = info.get(key, None)
+            if value is not None:
+                try:
+                    if isinstance(value, str):
+                        value = float(value.replace("%", "").strip())
+
+                    if "Gross Margin" in label:
+                        value = f"{value * 100:.1f}%"
+                    elif "Market Cap" in label:
+                        value = (
+                            f"{value/1e9:.2f} B"
+                            if value >= 1e9
+                            else (f"{value/1e6:.2f} M" if value >= 1e6 else value)
+                        )
+                    else:
+                        value = round(value, 2)
+                except ValueError:
+                    value = "N/A"  # If the value can't be converted to float
+
+                col.metric(label=label, value=value)
+            else:
+                col.metric(label=label, value="N/A")
+
+    except Exception as e:
+        st.error(f"Failed to display ticker metrics for '{ticker}': {e}")
+        logging.error(f"Error in display_ticker_metrics: {e}")
+
+
+def plot_candle_chart_with_volume_profile(data, va_high, va_low, poc_price):
+    try:
+        # Create lines for Value Area High, Value Area Low, and Point of Control
+        poc_line = pd.Series(poc_price, index=data.index)
+        va_high_line = pd.Series(va_high, index=data.index)
+        va_low_line = pd.Series(va_low, index=data.index)
+
+        # Add these lines to the candlestick plot
+        apds = [
+            mpf.make_addplot(
+                poc_line, type="line", color="red", linestyle="dashed", width=3
+            ),
+            mpf.make_addplot(
+                va_high_line, type="line", color="blue", linestyle="dashed", width=0.7
+            ),
+            mpf.make_addplot(
+                va_low_line, type="line", color="blue", linestyle="dashed", width=0.7
+            ),
+        ]
+
+        fig, ax = mpf.plot(
+            data,
+            type="candle",
+            addplot=apds,
+            style="yahoo",
+            volume=True,
+            show_nontrading=False,
+            returnfig=True,
+        )
+        st.pyplot(fig)
+
+    except Exception as e:
+        st.error(f"Failed to plot candlestick chart for the ticker: {e}")
+        logging.error(f"Error in plot_candle_chart_with_volume_profile: {e}")
+
+
+def display_sentiment_gauge(news_data, total_polarity):
+    try:
+        if len(news_data) > 0:
+            average_sentiment = total_polarity / len(news_data)
+            color = (
+                "green"
+                if average_sentiment >= 0.5
+                else ("orange" if average_sentiment >= 0 else "red")
+            )
+
+            fig = go.Figure(
+                go.Indicator(
+                    mode="gauge+number",
+                    value=average_sentiment,
+                    domain={"x": [0, 1], "y": [0, 1]},
+                    gauge={"axis": {"range": [-1, 1]}, "bar": {"color": color}},
+                )
+            )
+            fig.update_layout(width=300, height=300)
+            st.plotly_chart(fig)
+        else:
+            st.write("No sentiment or news data available.")
+    except Exception as e:
+        st.error(f"Failed to display sentiment gauge: {e}")
+        logging.error(f"Error in display_sentiment_gauge: {e}")
+
 
 def main():
     init_session_state()
+
+    # Ensure the session state is updated if the search button is clicked
+    if search_button_1:
+        st.session_state.search_clicked = True
+        st.session_state.search_ticker = search_ticker_1
+    else:
+        st.session_state.search_clicked = False
 
     if not st.session_state.data_loaded:
         file_path = "tickers.csv"
         df = pd.read_csv(file_path)
         st.session_state.companies = df["ticker"].tolist()
         st.session_state.data_loaded = True
-
         st.session_state.metrics = fetch_metrics_data(st.session_state.companies)
+
+    # Early exit to search and display only the chart for the specific ticker
+    if st.session_state.search_clicked and st.session_state.search_ticker:
+        tickers = [
+            ticker.strip() for ticker in st.session_state.search_ticker.split(";")
+        ]
+        start_date_str, end_date_str = get_date_range(days_history)
+
+        for ticker in tickers:
+            search_ticker_and_display(
+                ticker,
+                start_date_str,
+                end_date_str,
+                option,  # Removed st.session_state.combined_metrics
+            )
+        return
 
     if not st.session_state.companies:
         st.info("No companies found in the uploaded file.")
@@ -134,12 +360,12 @@ def main():
         peg_threshold_high,
         gross_margin_threshold,
     )
-    
+
     if "company" in filtered_companies_df.columns:
         filtered_company_symbols = filtered_companies_df["company"].tolist()
     else:
         st.error("The expected 'company' column was not found.")
-        return 
+        return
 
     metrics_filtered = fetch_additional_metrics_data(filtered_company_symbols)
 
@@ -151,7 +377,7 @@ def main():
     final_shortlist_labels = []
 
     st.markdown("# Analysis Results - Short List")
-    
+
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
@@ -163,17 +389,71 @@ def main():
         )
 
     df = pd.DataFrame(st.session_state.combined_metrics)
-    
-    columns_to_display = ['company_labels', 'shortName', 'sector', 'industry', 'fullTimeEmployees','overallRisk', 'opCashflow', 'repurchaseCapStock']
-    filtered_df: pd.DataFrame = df[columns_to_display].copy()
-    filtered_df['opCashflow'] = filtered_df['opCashflow'].apply(lambda x: replace_with_zero(x))
-    filtered_df['repurchaseCapStock'] = filtered_df['repurchaseCapStock'].apply(lambda x: replace_with_zero(x))
 
-    # assert len(metrics_filtered['freeCashflow']) == len(filtered_df), "Mismatch in number of records"
+    columns_to_display = [
+        "company_labels",
+        "shortName",
+        "sector",
+        "industry",
+        "fullTimeEmployees",
+        "overallRisk",
+        "opCashflow",
+        "repurchaseCapStock",
+    ]
+    filtered_df = df[columns_to_display].copy()
+    filtered_df["opCashflow"] = filtered_df["opCashflow"].apply(
+        lambda x: replace_with_zero(x)
+    )
+    filtered_df["repurchaseCapStock"] = filtered_df["repurchaseCapStock"].apply(
+        lambda x: replace_with_zero(x)
+    )
 
-    # THIS IS A QUICK FIX FOR MALFOMED freeCashflow inside  build_combined_metrics() - TBF later
-    # filtered_df['freeCashflow'] = metrics_filtered['freeCashflow']
-    filtered_df['repurchaseCapStock'] = filtered_df['repurchaseCapStock'].apply(lambda x: [-y for y in x] if isinstance(x, list) else -x)
+    filtered_df["fullTimeEmployees"] = (
+        pd.to_numeric(filtered_df["fullTimeEmployees"], errors="coerce")
+        .fillna(0)
+        .astype(int)
+    )
+    filtered_df["overallRisk"] = (
+        pd.to_numeric(filtered_df["overallRisk"], errors="coerce")
+        .fillna(0)
+        .astype(float)
+    )
+
+    sectors = filtered_df["sector"].unique().tolist()
+    industries = filtered_df["industry"].unique().tolist()
+
+    selected_sector = st.selectbox("Filter by Sector:", ["All"] + sectors)
+    selected_industry = st.selectbox("Filter by Industry:", ["All"] + industries)
+    min_employees, max_employees = st.slider(
+        "Filter by Full Time Employees:",
+        0,
+        int(filtered_df["fullTimeEmployees"].max()),
+        (0, int(filtered_df["fullTimeEmployees"].max())),
+    )
+    min_risk, max_risk = st.slider(
+        "Filter by Overall Risk:",
+        float(filtered_df["overallRisk"].min()),
+        float(filtered_df["overallRisk"].max()),
+        (
+            float(filtered_df["overallRisk"].min()),
+            float(filtered_df["overallRisk"].max()),
+        ),
+    )
+
+    if selected_sector != "All":
+        filtered_df = filtered_df[filtered_df["sector"] == selected_sector]
+
+    if selected_industry != "All":
+        filtered_df = filtered_df[filtered_df["industry"] == selected_industry]
+
+    filtered_df = filtered_df[
+        (filtered_df["fullTimeEmployees"] >= min_employees)
+        & (filtered_df["fullTimeEmployees"] <= max_employees)
+    ]
+    filtered_df = filtered_df[
+        (filtered_df["overallRisk"] >= min_risk)
+        & (filtered_df["overallRisk"] <= max_risk)
+    ]
 
     st.dataframe(
         filtered_df,
@@ -183,15 +463,15 @@ def main():
             "shortName": st.column_config.TextColumn("Short Name"),
             "sector": st.column_config.TextColumn("Sector"),
             "industry": st.column_config.TextColumn("Industry"),
-            "fullTimeEmployees": st.column_config.TextColumn("fullTimeEmployees"),
+            "fullTimeEmployees": st.column_config.TextColumn("Full Time Employees"),
             "overallRisk": st.column_config.TextColumn("Overall Risk"),
             # "freeCashflow": st.column_config.LineChartColumn(
             #     "Free Cashflow (4y)", y_min=-200, y_max=200
             # ),
-            "opCashflow": st.column_config.LineChartColumn(
+            "opCashflow": st.column_config.BarChartColumn(
                 "Operating Cashflow (4y)", y_min=-100, y_max=100
             ),
-            "repurchaseCapStock": st.column_config.LineChartColumn(
+            "repurchaseCapStock": st.column_config.BarChartColumn(
                 "Stock Repurchase Value (4y)", y_min=-50, y_max=50
             ),
         },
@@ -214,6 +494,7 @@ def main():
         for label in final_shortlist_labels
         if label in st.session_state.combined_metrics["company_labels"]
     ]
-    
+
+
 if __name__ == "__main__":
     main()
